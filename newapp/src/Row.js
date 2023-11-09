0fbe3f7252a67ff3f5935873c278c032d7456516
Thirunavukarasu <113583813+Thirunavukarasu17@users.noreply.github.com>
2023-11-09 21:35:43 +0530

    Update Row.js

--- newapp/src/Row.js
+++ newapp/src/Row.js
@@ -1,12 +1,9 @@
 import React, {useEffect, useState } from 'react'
 import axios from './axios'
-import YouTube from 'react-youtube'
-import movieTrailer from "movie-trailer"
 import "./Row.css"
 
 const  Row= ({title,fetchUrl,cineWaveOriginals}) => {
     const [movies,setMovies]=useState([])
-    const [trailerUrl,setTrailerUrl]=useState("")
 
     const base_Url="https://image.tmdb.org/t/p/original/"
 
@@ -19,26 +16,7 @@ const  Row= ({title,fetchUrl,cineWaveOriginals}) => {
         fetchdata()
     },[fetchUrl])
 
-    const opts={
-        height:"390",
-        width:"100%",
-        playerVars:{
-            autoplay:1
-        }
-    }
-    const handleClick=(movie)=>{
-        if(trailerUrl){
-            setTrailerUrl('')
-        }else{
-            movieTrailer(movie?.name||"")
-            .then(url=>{
-                const urlParams=new URLSearchParams(new URL(url).search)
-                setTrailerUrl(urlParams.get("v"))
-            })
-            .catch((err)=>console.log(err))
-        }
-    }
-
+    
   return (
     <div className='row'>
         <h2>{title}</h2>
@@ -47,13 +25,11 @@ const  Row= ({title,fetchUrl,cineWaveOriginals}) => {
                 (<img 
                     key={movie.id}
                     className={`row-poster ${cineWaveOriginals && "row-posterLarge"}`} 
-                    onClick={()=>handleClick(movie)}
+                    
                     src={`${base_Url}${cineWaveOriginals?movie.poster_path:movie.backdrop_path}`}
                     alt={movie.name}/>)
             )}
         </div>
-        {trailerUrl&&<YouTube videoId={trailerUrl} opts={opts}/>}
-    
     </div>
 
   )
